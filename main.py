3874c167d3f43594c0f5bf9c18dd73fc1e0454c8
jamestitus299 <titusjames299@gmail.com>
2024-07-06 10:17:46 +0530

    model creatd

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 
+from .models import User
+
+
+# API
 app = FastAPI()
 
+
+users = []
+
+
+
 @app.get("/")
-async def root():
+async def home():
     return "Hello World"
+
+@app.get("/users")
+async def get_users():
+    return users
+
+
+@app.post("/users")
+async def post_users(user : User):
+    users.append(user)
+    return {"message": "added"}
